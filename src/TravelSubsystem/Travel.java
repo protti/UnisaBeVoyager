0759e2a5bddd67eeb4bb49942b1f1ef4ddb556cf
Gifted <donatotiano@gmail.com>
2016-12-29 17:10:54 +0100

    Update Travel

--- src/TravelSubsystem/Travel.java
+++ src/TravelSubsystem/Travel.java
@@ -10,12 +10,53 @@ import RouteSubsystem.Route;
 public class Travel {
 
 	private RegisteredUser creatoreViaggio;
-	Collection<RegisteredUser> partecipantiViaggio;
-	Collection<Poll> pollList;
-	Route route;
+	private Collection<RegisteredUser> partecipantiViaggio;
+	private Collection<Poll> pollList;
+	private Route route;
 	private int id;
 	private String startDate;
 	private String endDate;
+	public Travel(RegisteredUser creatoreViaggio, int id, String startDate, String endDate, boolean type) {
+
+		this.creatoreViaggio = creatoreViaggio;
+		this.id = id;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.type = type;
+	}
+
+	public Travel(RegisteredUser creatoreViaggio, String startDate, String endDate, boolean type) {
+		this.creatoreViaggio = creatoreViaggio;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.type = type;
+	}
+
+	public RegisteredUser getCreatoreViaggio() {
+		return creatoreViaggio;
+	}
+
+	public Collection<RegisteredUser> getPartecipantiViaggio() {
+		return partecipantiViaggio;
+	}
+
+	public void setPartecipantiViaggio(Collection<RegisteredUser> partecipantiViaggio) {
+		this.partecipantiViaggio = partecipantiViaggio;
+	}
+
+	public Collection<Poll> getPollList() {
+		return pollList;
+	}
+
+	public void setPollList(Collection<Poll> pollList) {
+		this.pollList = pollList;
+	}
+
+	public void setRoute(Route route) {
+		this.route = route;
+	}
+
+
 	private boolean type;
 
 	public int getId() {
@@ -65,5 +106,14 @@ public class Travel {
 	public void setType(boolean type) {
 		this.type = type;
 	}
+
+
+	public Route getRoute()
+	{
+		return this.route;
+	}
+
+
+
 
 }
