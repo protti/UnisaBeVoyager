ee12e8d3f58213103b50f94fb361560e0e193a15
PZirp <p.zirpoli3@studenti.unisa.it>
2016-12-29 16:53:15 +0100

    Creato FeedbackSubsystem
    Creato package FeedbackSubsystem con classe base Feedback

---
+++ src/FeedbackSubsystem/Feedback.java
@@ -0,0 +1,33 @@
+package FeedbackSubsystem;
+
+public class Feedback {
+
+	private int id;
+	private String sender;
+	private String message;
+	private int date;
+	
+		public Feedback(int id, String sender, String message, int date) {
+		super();
+		this.id = id;
+		this.sender = sender;
+		this.message = message;
+		this.date = date;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public int getDate() {
+		return date;
+	}
+	
+	
+	
+}
