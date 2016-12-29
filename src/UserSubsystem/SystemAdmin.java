efec260569a8e3fe429bd1bf3a646554c2e932a5
PZirp <p.zirpoli3@studenti.unisa.it>
2016-12-29 16:50:34 +0100

    Classe SystemAdmin
    Aggiunta classe SystemAdmin

---
+++ src/UserSubsystem/SystemAdmin.java
@@ -0,0 +1,64 @@
+package UserSubsystem;
+
+public class SystemAdmin extends User {
+
+	private String email;
+	private String password;
+	private String nome;
+	private String cognome;
+
+	public String getEmail() {
+		// TODO - implement {class}.{operation}
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		// TODO - implement {class}.{operation}
+		throw new UnsupportedOperationException();
+	}
+
+	public String getPassword() {
+		// TODO - implement {class}.{operation}
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param password
+	 */
+	public void setPassword(String password) {
+		// TODO - implement {class}.{operation}
+		throw new UnsupportedOperationException();
+	}
+
+	public String getNome() {
+		// TODO - implement {class}.{operation}
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nome
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		// TODO - implement {class}.{operation}
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cognome
+	 */
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+}
