98e68d058257624cb1ed015425ca85b35470b6fb
PZirp <p.zirpoli3@studenti.unisa.it>
2016-12-29 16:52:40 +0100

    Creato LocationSubsystem
    Creato package LocationSubsystem con classe base Location

---
+++ src/LocationSubsystem/Location.java
@@ -0,0 +1,37 @@
+package LocationSubsystem;
+
+public class Location {
+
+	private String name;
+	private int id;
+	private String descrizione;
+	
+	public Location(String name, int id, String descrizione) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.descrizione = descrizione;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	
+	
+}
