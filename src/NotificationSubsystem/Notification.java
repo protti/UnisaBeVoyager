dda2facd9158daca291ad2ba13743dd34ebb1794
PZirp <p.zirpoli3@studenti.unisa.it>
2016-12-29 16:52:20 +0100

    Creaeto NotificationSubsystem
    Creato package NotificationSubsystem con classe base Notification

---
+++ src/NotificationSubsystem/Notification.java
@@ -0,0 +1,86 @@
+package NotificationSubsystem;
+
+import UserSubsystem.RegisteredUser;
+
+public class Notification {
+
+	private int id;
+	private RegisteredUser sender;
+	private RegisteredUser recipient;
+	private String body;
+	private boolean read;
+	private String sendDate;
+
+	public int getId() {
+		return this.id;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public RegisteredUser getSender() {
+		return this.sender;
+	}
+
+	/**
+	 * 
+	 * @param sender
+	 */
+	public void setSender(RegisteredUser sender) {
+		this.sender = sender;
+	}
+
+	public RegisteredUser getRecipient() {
+		return this.recipient;
+	}
+
+	/**
+	 * 
+	 * @param recipient
+	 */
+	public void setRecipient(RegisteredUser recipient) {
+		this.recipient = recipient;
+	}
+
+	public String getBody() {
+		return this.body;
+	}
+
+	/**
+	 * 
+	 * @param body
+	 */
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public boolean getReadType() {
+		return this.read;
+	}
+
+	/**
+	 * 
+	 * @param readType
+	 */
+	public void setReadType(boolean readType) {
+		this.read = read;
+	}
+
+	public String getSendDate() {
+		return this.sendDate;
+	}
+
+	/**
+	 * 
+	 * @param sendDate
+	 */
+	public void setSendDate(String sendDate) {
+		this.sendDate = sendDate;
+	}
+
+}
