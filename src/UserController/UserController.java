e66231eb110f5e26010e5780b17e24a2f69b2eb6
Gifted <donatotiano@gmail.com>
2016-12-30 16:39:20 +0100

    Created UserController

---
+++ src/UserController/UserController.java
@@ -0,0 +1,38 @@
+package UserController;
+
+import java.util.GregorianCalendar;
+
+import UserSubsystem.RegisteredUser;
+import UserSubsystem.User;
+
+public class UserController {
+
+		public User fetchUser(int id)
+		{
+		
+			return null;
+		}
+	
+		public RegisteredUser searchUser(String username)
+		{
+		
+			return null;
+		}
+		
+		public RegisteredUser createUser(String email, String username, String password, String nome, String cognome,
+				GregorianCalendar birthDate, int age)
+		{
+			return null;
+		}
+		
+		public RegisteredUser getUser(String username,String password)
+		{
+			return null;
+		}
+		
+		public boolean deleteUser(int id)
+		{
+			return false;
+		}
+	
+}
