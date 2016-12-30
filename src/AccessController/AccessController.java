86fe74ddacb768c6ec20e6d18f73321fb1393425
Gifted <donatotiano@gmail.com>
2016-12-30 17:18:33 +0100

    Update UserController and create AccessController and FeedbackController

---
+++ src/AccessController/AccessController.java
@@ -0,0 +1,31 @@
+package AccessController;
+
+import UserSubsystem.RegisteredUser;
+
+public class AccessController {
+
+
+
+	static public RegisteredUser recoveryPwd(String email)
+	{
+		return null;
+	}
+	static public String updatePassword(String email,String password)
+	{
+		return null;
+	}
+	static public boolean controlLog(int idUser)
+	{
+		return false;
+	}
+	static public boolean controlLogOut(int idUser)
+	{
+		return false;
+	}
+	static public RegisteredUser logUser(String username,String password)
+	{
+		return null;
+	}
+
+
+}
---
+++ src/FeedbackController/FeedbackController.java
@@ -0,0 +1,30 @@
+package FeedbackController;
+
+import java.util.GregorianCalendar;
+
+import FeedbackSubsystem.Feedback;
+
+public class FeedbackController {
+
+	
+	static public Feedback createFeedback(String sender, String message, GregorianCalendar date)
+	{
+		return null;
+	}
+	static public boolean deleteFeedbackRoute(int feedbackID)
+	{
+		return false;
+	}
+	static public boolean deleteFeedbackLocation(int feedbackID)
+	{
+		return false;
+	}
+	static public boolean deleteFeedbackUser(int feedbackID)
+	{
+		return false;
+	}
+	
+	
+	
+	
+}
