9ccf0fd4e039b1cb77fb10ad592148068e6b7789
PZirp <p.zirpoli3@studenti.unisa.it>
2016-12-31 19:03:47 +0100

    Spostata la classe UserController in package UserSubystem

---
+++ src/UserSubsystem/UserController.java
@@ -0,0 +1,35 @@
+package UserSubsystem;
+
+import java.util.GregorianCalendar;
+
+public class UserController {
+
+		static public User fetchUser(int id)
+		{
+		
+			return null;
+		}
+	
+		static public RegisteredUser searchUser(String username)
+		{
+		
+			return null;
+		}
+		
+		static public RegisteredUser createUser(String email, String username, String password, String nome, String cognome,
+				GregorianCalendar birthDate, int age)
+		{
+			return null;
+		}
+		
+		static public RegisteredUser getUser(String username,String password)
+		{
+			return null;
+		}
+		
+		static public boolean deleteUser(int id)
+		{
+			return false;
+		}
+	
+}
