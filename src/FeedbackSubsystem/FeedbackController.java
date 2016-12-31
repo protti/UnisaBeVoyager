7ad822c539b14f2cdc1d20f12571db6f09b5b412
PZirp <p.zirpoli3@studenti.unisa.it>
2016-12-31 20:17:04 +0100

    Spostata la classe FeedbackController in package FeedbackSubystem

---
+++ src/FeedbackSubsystem/FeedbackController.java
@@ -0,0 +1,28 @@
+package FeedbackSubsystem;
+
+import java.util.GregorianCalendar;
+
+public class FeedbackController {
+
+	
+	static public Feedback createFeedback(String sender, String message, GregorianCalendar date)
+	{
+		return null;
+	}	
+	static public boolean deleteFeedbackRoute(int feedbackID)
+	{
+		return false;
+	}
+	static public boolean deleteFeedbackLocation(int feedbackID)
+	{
+		return false;
+	}
+	static public boolean deleteFeedbackUser(int feedbackID)
+	{
+		return false;
+	}
+	
+	
+	
+	
+}
