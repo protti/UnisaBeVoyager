f39714069898886a42fffe527b2626a9d0563514
PZirp <p.zirpoli3@studenti.unisa.it>
2017-01-06 04:25:20 +0100

    Implementazione iniziale della conferma di un viaggio (aggiungere redirect per risultato positivo)

---
+++ src/servletTravel/CloseTravel.java
@@ -0,0 +1,61 @@
+package servletTravel;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import TravelSubsystem.Travel;
+import TravelSubsystem.TravelController;
+import UserSubsystem.RegisteredUser;
+
+/**
+ * Servlet implementation class CloseTravel
+ */
+@WebServlet("/CloseTravel")
+public class CloseTravel extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CloseTravel() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int travelID = Integer.parseInt(request.getParameter("travelID"));
+		
+		HttpSession session = request.getSession();		
+		RegisteredUser user = (RegisteredUser) session.getAttribute("user");
+		
+		Travel travel = TravelController.getTravel(travelID);
+		
+		RegisteredUser creator = travel.getCreatoreViaggio();
+		if(creator.getId() == user.getId() && travel != null) {
+			boolean b = TravelController.confirmTravel(travel);
+			if(b == false) {
+				response.sendRedirect("500page.html");
+				return;
+			} else {
+				//Mostrare un risultato positivo o fare qualcosa del genere
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
