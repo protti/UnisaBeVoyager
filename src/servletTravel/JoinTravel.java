11887cd5719de5244869ea8e77ca0209c4360ccb
PZirp <p.zirpoli3@studenti.unisa.it>
2017-01-06 04:26:40 +0100

    Implementazione iniziale della servlet per aggiungere un utente ad un viaggio

---
+++ src/servletTravel/JoinTravel.java
@@ -0,0 +1,59 @@
+package servletTravel;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import TravelSubsystem.Travel;
+import TravelSubsystem.TravelController;
+import UserSubsystem.RegisteredUser;
+
+/**
+ * Servlet implementation class JoinTravel
+ */
+@WebServlet("/JoinTravel")
+public class JoinTravel extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JoinTravel() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int travelID = Integer.parseInt(request.getParameter("travelID"));
+		
+		HttpSession session = request.getSession();		
+		RegisteredUser user = (RegisteredUser) session.getAttribute("user");
+		
+		Travel travel = TravelController.getTravel(travelID);
+		
+		if(travel != null) {
+			boolean b = TravelController.addUserInTravel(user, travel);
+			if(b == false) {
+				response.sendRedirect("500page.html");
+				return;
+			} else {
+				//Mostrare un risultato positivo o fare qualcosa del genere
+			}
+		}	}
+
+}
