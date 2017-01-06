7c0bc60d7957028c2d5560ab479386f6f64f38d5
PZirp <p.zirpoli3@studenti.unisa.it>
2017-01-06 04:24:49 +0100

    Implementazione iniziale della servlet per selezionare l'itinerario di un viaggio

---
+++ src/servletTravel/addRouteForNewTravel.java
@@ -0,0 +1,62 @@
+package servletTravel;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import LocationSubsystem.Location;
+import LocationSubsystem.LocationController;
+import RouteSubsystem.Route;
+import RouteSubsystem.RouteController;
+
+/**
+ * Servlet implementation class addRouteForNewTravel
+ */
+@WebServlet("/addRouteForNewTravel")
+public class addRouteForNewTravel extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public addRouteForNewTravel() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int routeID = Integer.parseInt(request.getParameter("id"));
+
+		Route route = RouteController.getRoute(routeID) ;
+		
+		if(route == null) {
+			response.sendRedirect("500page.html");
+			return;
+		}
+		else {
+			HttpSession session = request.getSession();
+			synchronized(session) {
+				session.setAttribute("selectedRoute", route);
+			}
+		}			
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
