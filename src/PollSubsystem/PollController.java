ea69dc928312643af72baf2543e39f6081dcd869
PZirp <p.zirpoli3@studenti.unisa.it>
2017-01-06 17:47:20 +0100

    Corretto il costruttore e corretto il metodo per l'update

--- src/PollSubsystem/PollController.java
+++ src/PollSubsystem/PollController.java
@@ -1,15 +1,13 @@
 package PollSubsystem;
 
 import java.sql.SQLException;
-import java.util.GregorianCalendar;
 
 import DBConnection.DBException;
 
 public class PollController {
-	public static Poll createPoll(String description,int pos, int neg, 
-			String startDate, String endDate,int travelId){
+	public static Poll createPoll(String description, String startDate, String endDate,int travelId){
 
-		Poll poll = new Poll(description,neg,pos,startDate,endDate);
+		Poll poll = new Poll(description, startDate, endDate);
 		try {
 			PollManager.savePollToDB(poll, travelId);
 		} catch (SQLException e) {
@@ -26,9 +24,11 @@ public class PollController {
 		try {
 			if(!PollManager.checkUserPoll(pollId, userId)){
 				Poll poll = PollManager.searchPollById(pollId);
-				if(voto >= 0) poll.setVpositive(poll.getVpositive() + 1);
-				else poll.setVnegative(poll.getVnegative() + 1);
+				/*if(voto >= 0) poll.setVpositive(poll.getVpositive() + 1);
+				else poll.setVnegative(poll.getVnegative() + 1);*/
+				poll.registerVote(voto);
 				PollManager.updatePoll(poll);
+				PollManager.insertUserPoll(pollId, userId);
 				return true;
 			} 
 			else return false;
